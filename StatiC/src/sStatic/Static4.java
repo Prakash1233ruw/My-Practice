ae8e4f74259b945e3e4aceaa21b748a87b5ed222
prakash <pr0018908@gmail.com>
2023-12-22 23:19:03 +0530

    new static

---
+++ StatiC/src/sStatic/Static4.java
@@ -0,0 +1,29 @@
+package sStatic;
+
+class SomeClass
+{
+	static 
+	{
+		System.out.println("Static block in some other class not in main");
+	}
+	
+	static void disp()
+	{
+		System.out.println("disp");
+	}
+	
+}
+public class Static4 
+{
+	static
+	{
+		System.out.println("static block in main method class");
+	}
+
+	public static void main(String[] args) 
+	{
+		SomeClass.disp();
+
+	}
+
+}
